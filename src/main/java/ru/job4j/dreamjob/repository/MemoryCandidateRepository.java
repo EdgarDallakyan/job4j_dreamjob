2b527222ece503d5e9da9e5fb797c8658614bde9
edgar <edgar_dallakyan@mail.ru>
2025-05-14 13:24:04 +0300

    2. Формы. Списки. [#504854 #543182]

--- src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
@@ -20,12 +20,12 @@ public class MemoryCandidateRepository implements CandidateRepository {
     private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
     private MemoryCandidateRepository() {
-        save(new Candidate(0, "Candidate1", "Description1", LocalDateTime.now(), 1));
-        save(new Candidate(0, "Candidate2", "Description2", LocalDateTime.now(), 2));
-        save(new Candidate(0, "Candidate3", "Description3", LocalDateTime.now(), 3));
-        save(new Candidate(0, "Candidate4", "Description4", LocalDateTime.now(), 4));
-        save(new Candidate(0, "Candidate5", "Description5", LocalDateTime.now(), 5));
-        save(new Candidate(0, "Candidate6", "Description6", LocalDateTime.now(), 6));
+        save(new Candidate(0, "Candidate1", "Description1", LocalDateTime.now(), 2));
+        save(new Candidate(0, "Candidate2", "Description2", LocalDateTime.now(), 3));
+        save(new Candidate(0, "Candidate3", "Description3", LocalDateTime.now(), 1));
+        save(new Candidate(0, "Candidate4", "Description4", LocalDateTime.now(), 2));
+        save(new Candidate(0, "Candidate5", "Description5", LocalDateTime.now(), 1));
+        save(new Candidate(0, "Candidate6", "Description6", LocalDateTime.now(), 3));
     }
 
     @Override
